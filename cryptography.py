4ca0c06261ef8d84b208ae9d3b2b587648a7ee73
anoushkaalavilli <anoushkaalavilli@hanovernorwichschools.org>
2015-10-23 14:56:03 -0400

    Updated from Brython Server: 10/23/2015 2:56:03 PM

--- cryptography.py
+++ cryptography.py
@@ -10,8 +10,18 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-text= input("Please enter a string. ")
-listnums= []
-for i in (text):
-    listnums.append(associations.find(i))
-print (listnums)
+message= input("Please enter a message. ")
+key= input("Please enter a key. ")
+listnumsofmessage= []
+listnumsofkey= []
+
+for i in (message):
+    listnumsofmessage.append(associations.find(i))
+print (listnumsofmessage)
+
+for i in (key):
+    listnumsofkey.append(associations.find(i))
+print (listnumsofkey)
+
+for (listnumsofmessage[i]) and (listnumsofkey[i])
+    
