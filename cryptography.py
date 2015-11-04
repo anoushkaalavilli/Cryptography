de5095f87538ea64246413ffa35f0e1e7e08459b
anoushkaalavilli <anoushkaalavilli@hanovernorwichschools.org>
2015-11-04 17:48:09 -0500

    Updated from Brython Server: 11/4/2015 5:48:08 PM

--- cryptography.py
+++ cryptography.py
@@ -9,12 +9,11 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
-input1= (input("Enter e to encrypt, d to decrypt, or q to quit: "))
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-#message= input("Message: ")
+#input1= (input("Enter e to encrypt, d to decrypt, or q to quit: "))
+
 #key= input("Key: ")
-listnumsofmessage= []
-listnumsofkey= []
+#listnumsofmessage= []
+#listnumsofkey= []
 
 #for i in (message):
     #listnumsofmessage.append(associations.find(i))
@@ -24,88 +23,97 @@ listnumsofkey= []
     #listnumsofkey.append(associations.find(i))
 #print (listnumsofkey)
 
-
-if (input1)==("e"):
-    message= input("Message: ")
-    key= input("Key: ")
+while (1==1):
+    input1= (input("Enter e to encrypt, d to decrypt, or q to quit: "))
+    associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+#message= input("Message: ")
+#key= input("Key: ")
     listnumsofmessage= []
     listnumsofkey= []
+    if (input1)==("e"):
+        message= input("Message: ")
+        key= input("Key: ")
+        listnumsofmessage= []
+        listnumsofkey= []
+        
+        for i in (message):
+            listnumsofmessage.append(associations.find(i))
+        #print (listnumsofmessage)
+        
+        for i in (key):
+            listnumsofkey.append(associations.find(i))
+        #print (listnumsofkey)
+        keylength = len(key)
+        messagelength = len(message)
+        increasefactor = 1 + messagelength//keylength
+        key= increasefactor*key
+        #print (message, key)
+        sumval= []
+        #for i in (range(messagelength)):
+            #print (message[i], key[i])
     
-    for i in (message):
-        listnumsofmessage.append(associations.find(i))
-    #print (listnumsofmessage)
+        for i in (key):
+            listnumsofkey.append(associations.find(i))
+        #print ((listnumsofmessage), (listnumsofkey))
     
-    for i in (key):
-        listnumsofkey.append(associations.find(i))
-    #print (listnumsofkey)
-    keylength = len(key)
-    messagelength = len(message)
-    increasefactor = 1 + messagelength//keylength
-    key= increasefactor*key
-    #print (message, key)
-    sumval= []
-    #for i in (range(messagelength)):
-        #print (message[i], key[i])
-
-    for i in (key):
-        listnumsofkey.append(associations.find(i))
-    #print ((listnumsofmessage), (listnumsofkey))
-
-    numbersinmessage= (len(listnumsofmessage))
-    #print (numbersinmessage)
-    encrypted= []
-    for i in range (numbersinmessage):
-        placeinassociations= ((listnumsofmessage[i]) + (listnumsofkey[i]))
-        if placeinassociations > 84:
-            placeinassociations= ((placeinassociations) - 85)
-        encrypted.append(associations[placeinassociations])
-    #print (encrypted)
-    str2= ""
-    for i in (encrypted):
-        str2= str2+(i)
-    print (str2)
-elif (input1)==("d"):
-    message= input("Message: ")
-    key= input("Key: ")
-    keylength = len(key)
-    messagelength = len(message)
-    increasefactor = 1 + messagelength//keylength
-    key= increasefactor*key
-    listnumsofmessage= []
-    listnumsofkey= []
+        numbersinmessage= (len(listnumsofmessage))
+        #print (numbersinmessage)
+        encrypted= []
+        for i in range (numbersinmessage):
+            placeinassociations= ((listnumsofmessage[i]) + (listnumsofkey[i]))
+            if placeinassociations > 84:
+                placeinassociations= ((placeinassociations) - 85)
+            encrypted.append(associations[placeinassociations])
+        #print (encrypted)
+        str2= ""
+        for i in (encrypted):
+            str2= str2+(i)
+        print (str2)
+    elif (input1)==("d"):
+        message= input("Message: ")
+        key= input("Key: ")
+        keylength = len(key)
+        messagelength = len(message)
+        increasefactor = 1 + messagelength//keylength
+        key= increasefactor*key
+        listnumsofmessage= []
+        listnumsofkey= []
+        
+        for i in (message):
+            listnumsofmessage.append(associations.find(i))
+        #print (listnumsofmessage)
+        
+        for i in (key):
+            listnumsofkey.append(associations.find(i))
+        #print ("listnumsofkey=", listnumsofkey)
+        decryptnumslist= []
+        for i in (message):
+            decryptnumslist.append(associations.find(i))
+        #print ((decryptnumslist), (listnumsofkey))
+        numberindecryptlist= (len(decryptnumslist))
+        answerlist= []
+        for i in range(numberindecryptlist):
+            #print ("i=", i)
+            decrypt= ((decryptnumslist[i])-(listnumsofkey[i]))
+            if decrypt <0:
+                decrypt= ((decrypt)+85)
+            answerlist.append(decrypt)
+        #print (answerlist)
+        letteranswerlist= []
+        for i in (answerlist):
+            letteranswerlist.append(associations[i])
+        #print ((letteranswerlist))
+        str1= ""
+        for i in (letteranswerlist):
+            str1= str1+(i)
+        print (str1)
+    elif (input1)==("q"):
+        print ("Goodbye!")
+        break
+    else:
+        print ("Did not understand command, try again.")
+        
     
-    for i in (message):
-        listnumsofmessage.append(associations.find(i))
-    #print (listnumsofmessage)
-    
-    for i in (key):
-        listnumsofkey.append(associations.find(i))
-    #print ("listnumsofkey=", listnumsofkey)
-    decryptnumslist= []
-    for i in (message):
-        decryptnumslist.append(associations.find(i))
-    #print ((decryptnumslist), (listnumsofkey))
-    numberindecryptlist= (len(decryptnumslist))
-    answerlist= []
-    for i in range(numberindecryptlist):
-        #print ("i=", i)
-        decrypt= ((decryptnumslist[i])-(listnumsofkey[i]))
-        if decrypt <0:
-            decrypt= ((decrypt)+85)
-        answerlist.append(decrypt)
-    #print (answerlist)
-    letteranswerlist= []
-    for i in (answerlist):
-        letteranswerlist.append(associations[i])
-    #print ((letteranswerlist))
-    str1= ""
-    for i in (letteranswerlist):
-        str1= str1+(i)
-    print (str1)
-elif (input1)==("q"):
-    print ("Goodbye!")
-else:
-    print ("Did not understand command, try again.")
     
     
     
